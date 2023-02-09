33d933579ac7ef651d2b1f710a28bdce6d4a2fac
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2023-02-09 04:25:04 +0100

    Worked on login Response

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    console.log("Trying to Sign In");
+    console.log("Trying to Log In");
     var email = req.body.email;
     var password = req.body.password;
 
@@ -23,11 +23,13 @@ router.post("/", (req, res) => {
             console.log(err);
         }
         pool.query(searchQuery, (error, result, fields) => {
+            console.log(searchQuery)
             if (result[0] == null) {
                 res.status(401).send()
             }
             else {
-                res.status(201).send(JSON.stringify(result[0]));
+                console.log(email + "has Logged in");
+                res.status(201).send(JSON.parse(JSON.stringify(result[0])));
             }
 
         })
